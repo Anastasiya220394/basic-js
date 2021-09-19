7cdbe032935961d870e1a0f2b9abb9fc1aff71b7
Anastasiya Nitievskaya <anonim22@icloud.com>
2021-09-19 20:50:07 +0300

    feat: add vigenere function

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,67 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor (value){
+    if(value==true||value===undefined){this.value=true} else{this.value=false};        
+    this.table=[]    
+    let a= ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    this.alpha=a
+    for(let i=0;i<26;i++){
+      this.table[i]=[]
+      for(let j=0;j<26;j++){
+        this.table[i][j]=a[j]      
+      }
+     
+      a.push(a.shift())
+    }
+  }
+  encrypt(message,key) {
+      if(message===undefined||key===undefined){
+        throw new Error();
+      }      
+      message=message.toUpperCase()
+      key=key.toUpperCase()
+      let m=0
+      let k=0
+      let j=0
+      let result=[]
+        for (let i=0;i<message.length;i++){
+          if (this.alpha.indexOf(message[i])==-1){
+                      result.push(message[i])        
+          }else{     
+          m=this.alpha.indexOf(message[i])
+          k=this.alpha.indexOf(key[j])        
+          result.push(this.table[k][m])        
+          if(j==key.length-1) {j=0}else{j++}
+        }
+        }
+        if(this.value==false){result=result.reverse()}
+      return result.join("")
   }
   decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+          if(message===undefined||key===undefined){
+            throw new Error();     
+          }
+          message=message.toUpperCase()
+          key=key.toUpperCase()
+          let char=0
+          let m=0
+          let k=0
+          let j=0
+          let result=[]
+          for (let i=0;i<message.length;i++){
+            if (this.alpha.indexOf(message[i])==-1){
+                        result.push(message[i])        
+            }else{ 
+            k=this.alpha.indexOf(key[j]) 
+            char= this.table[k].indexOf(message[i])    
+            m=this.alpha[char]        
+            result.push(m)    
+            if(j==key.length-1) {j=0}else{j++}
+          }
+      }
+      if(this.value==false){result=result.reverse()}
+      return result.join("")
+    }
   }
-}
+
